2e81a0ea702f251ce5bdf64a1d269a8a66ae226f
MndTheNerd <mndthenerd@gmail.com>
2020-01-13 20:58:07 +0300

    added a Questions class, Updated the Static String with dynamic one depending on the questions available on click. also added a question checker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,28 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  //a list of questions
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
+  ];
+
+  //tracking the questions list
+  int questionNumber = 0;
+
+  //tracks the score of the user
+  List<Icon> scoreKeeper = [];
+  //function to check on the score of the user
+  void checkAnswer() {
+    setState(() {
+      questionNumber++;
+      print(questionNumber);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +84,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                if (questions[questionNumber].questionAnwser == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
                 //The user picked true.
+                checkAnswer();
               },
             ),
           ),
@@ -80,11 +109,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                if (questions[questionNumber].questionAnwser == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+                checkAnswer();
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+//        Row(
+//          children: <Widget>[scoreKeeper.first],
+//        ),
       ],
     );
   }
